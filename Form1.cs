5bfe177eabf74a7f660f1d30762af3957c121b6b
JideO <JideOyelayo1@Gmail.com>
2022-03-23 01:18:52 +0000

    -Need to add buttons -Add display of answer -add lives

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace HangMan
 {
     public partial class Form1 : Form
@@ -5,6 +7,40 @@ namespace HangMan
         public Form1()
         {
             InitializeComponent();
+            Setup();
         }
+        public static string ReplaceAtIndex(string text, int index, char c)
+        {
+            var stringBuilder = new StringBuilder(text);
+            stringBuilder[index] = c;
+            return stringBuilder.ToString();
+        }
+
+        public static List<char> Attempts = new List<char>();
+        public static string WordToGuess = "";
+
+        public static void Setup()
+        {
+            string[] words = { "Magic", "Banana", "Breach" };
+            Random idx = new Random();
+            WordToGuess = words[idx.Next(words.Length)].ToLower();
+        }
+
+        public static void Display()
+        {
+            string output = "";
+            var idx = 0;
+            foreach (char c in WordToGuess)
+            {
+                if (Attempts.Contains(c) && WordToGuess[idx] == c)
+                    output += c;
+                else
+                    output += "-";
+                idx++;
+            }
+
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -14,40 +14,11 @@ namespace HangMan
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-            Setup();
-        }
-        public static string ReplaceAtIndex(string text, int index, char c)
-        {
-            var stringBuilder = new StringBuilder(text);
-            stringBuilder[index] = c;
-            return stringBuilder.ToString();
-        }
-
-        public static List<char> Attempts = new List<char>();
-        public static string WordToGuess = "";
-
-        public static void Setup()
-        {
-            string[] words = { "Magic", "Banana","Breach" };
-            Random idx = new Random();
-            WordToGuess = words[idx.Next(words.Length)].ToLower();           
+            
         }
         
-        public static void Display()
-        {
-            string output = "";
-            var idx = 0;
-            foreach (char c in WordToGuess)
-            {
-                if (Attempts.Contains(c) && WordToGuess[idx] == c)
-                    output += c;
-                else
-                    output += "-";
-                idx++;
-            }
-            
-            
-        }
+
+        
 
 
 
