56e933464cf3f5df173eec8404e079402f0a855b
Jide Oyelayo <jide.oyelayo@idexbiometrics.com>
2022-07-08 13:38:55 +0100

    starts from the centre and changes mouse and colour change

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -392,6 +392,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(578, 272);
             this.Controls.Add(this.RestartButton);
             this.Controls.Add(this.HangManImages);
